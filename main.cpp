1a7dd37520ed6f467e89af6dae3f8535b1d89490
2481814748@qq.com <2481814748@qq.com>
2023-08-06 21:14:42 +0800

    test hook

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 // delete will cause error
-// /*
-// # Copyright (c) 2023 Zong
-// */
+/*
+# Copyright (c) 2023 Zong
+*/
 
 #include <iostream>
 
-int add(int a, int b) { return a + b; }
+int add_2(int a, int b) { return a + b; }
 
 int main() { return 0; }
